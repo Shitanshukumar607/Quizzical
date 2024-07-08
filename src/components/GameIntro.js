ffc8be8af794fd50dfc55c32555386d4587dd65d
Shitanshu <shitanshukumar639@gmail.com>
2024-07-08 17:41:34 +0530

    completed intro page

--- src/components/GameIntro.js
+++ src/components/GameIntro.js
@@ -1,23 +1,77 @@
-export default function GameIntro(props) {
-  return (
-    <section>
-      <h1>Quiziical</h1>
-      <p>Answer the questions and test your knowledge!</p>
+import { categories, difficulties, questionTypes } from "../data";
+import "../index.css";
 
-      <label>
-        Category
+export default function GameIntro(props) {
+  const categoryElements = categories.map((category, index) => {
+    return (
+      <option key={index} value={category.id}>
+        {category.name}
+      </option>
+    );
+  });
+
+  const difficultyElements = difficulties.map((difficulty, index) => {
+    return (
+      <option key={index} value={difficulty.id}>
+        {difficulty.name}{" "}
+      </option>
+    );
+  });
+
+  const questionTypesElements = questionTypes.map((questionType, index) => {
+    return (
+      <option key={index} value={questionType.id}>
+        {questionType.name}
+      </option>
+    );
+  });
+
+  return (
+    <section className="game-container">
+      <h1 className="game-title">Quiziical</h1>
+      <p className="game-description">
+        Answer the questions and test your knowledge!
+      </p>
+
+      <div className="selection-container">
+        <label htmlFor="category">Category</label>
         <select
           name="category"
           id="category"
           value={props.data.category}
           onChange={props.onChange}
         >
-          <option value="">Any Category</option>
-          <option value="1">one</option>
-          <option value="2">two</option>
+          {categoryElements}
         </select>
-      </label>
-      <button onClick={props.startGame}>Start Game</button>
+      </div>
+
+      <div className="selection-container">
+        <label htmlFor="difficulty">Difficulty </label>
+        <select
+          name="difficulty"
+          id="difficulty"
+          value={props.data.difficulty}
+          onChange={props.onChange}
+        >
+          {difficultyElements}
+        </select>
+      </div>
+
+      <div className="selection-container">
+        <label htmlFor="type">Types of questions</label>
+        <select
+          name="type"
+          id="type"
+          value={props.data.type}
+          onChange={props.onChange}
+        >
+          {questionTypesElements}
+        </select>
+      </div>
+
+      <button className="start-game-btn" onClick={props.startGame}>
+        Start Game
+      </button>
     </section>
   );
 }
