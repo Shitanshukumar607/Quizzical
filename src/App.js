60ed905ebbe5c87b3c177137bbc3bd4ca6a0db8b
Shitanshu <shitanshukumar639@gmail.com>
2024-07-08 11:13:36 +0530

    added blob component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useState, useEffect } from "react";
+import Blobs from "./components/Blobs";
+import "./index.css";
 function App() {
-  return <h1>This is a game</h1>;
+  let o;
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&category=27&difficulty=easy")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <Blobs />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Blobs.js
@@ -0,0 +1,8 @@
+export default function Blobs() {
+  return (
+    <>
+      <img className="blob-top" src="blobs/blob-top.svg" />
+      <img className="blob-bottom" src="blobs/blob-bottom.svg" />
+    </>
+  );
+}
