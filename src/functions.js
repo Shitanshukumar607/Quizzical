8908f5fb558ade9aee21e10a6c936854ef808283
Shitanshu <shitanshukumar639@gmail.com>
2024-07-09 20:49:08 +0530

    some common functions

---
+++ src/functions.js
@@ -0,0 +1,16 @@
+export default function shuffleArray(array, newElement) {
+  array.push(newElement);
+  let currentIndex = array.length;
+  let temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
