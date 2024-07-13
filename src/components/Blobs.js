180f780573a2cb8d21841b28fca883c47ba89228
Shitanshu <shitanshukumar639@gmail.com>
2024-07-13 21:25:51 +0530

    added alts to imgs

--- src/components/Blobs.js
+++ src/components/Blobs.js
@@ -1,8 +1,12 @@
 export default function Blobs() {
   return (
     <>
-      <img className="blob-top" src="blobs/blob-top.svg" />
-      <img className="blob-bottom" src="blobs/blob-bottom.svg" />
+      <img className="blob-top" src="blobs/blob-top.svg" alt="blob top" />
+      <img
+        className="blob-bottom"
+        src="blobs/blob-bottom.svg"
+        alt="blob bottom"
+      />
     </>
   );
 }
